32ee36027086e084cd9f3db2dd4decbb8ca60167
Nithin Peter <nithinpeter7@gmail.com>
2018-02-01 22:58:18 +1100

    Add background, logo and text.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,42 @@
-import React, { Component } from 'react';
+import React from 'react';
+import styled from 'styled-components';
+import tile from './tile.jpg';
+import logo from './logo.png';
 
-class App extends Component {
-  render() {
-    return <div />;
+const App = styled.div`
+  height: 100vh;
+  width: 100vw;
+  background-image: url(${tile});
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: relative;
+`;
+
+const Seven = styled.div`
+  height: 33%;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  background: rgba(0, 0, 0, 0.4);
+  img {
+    padding: 0 3vmin;
+    max-height: 25vmin;
   }
-}
+  h1 {
+    font-size: 10vmin;
+    margin: 0;
+    color: #ffffff;
+  }
+`;
 
-export default App;
+export default () => (
+  <App>
+    <Seven>
+      <img src={logo} alt="seven logo" />
+      <h1>Home and Away</h1>
+    </Seven>
+  </App>
+);
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactDOMServer from 'react-dom/server';
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders without crashing', () => {
+  expect(ReactDOMServer.renderToString(<App />)).toMatchSnapshot();
 });
